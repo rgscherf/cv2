31e594a6462918c8de353d6242406f847049fa52
rgscherf <rgscherf@gmail.com>
2016-07-13 17:06:04 -0400

    basic JSON responses

--- src/cv2/handler.clj
+++ src/cv2/handler.clj
@@ -1,9 +1,32 @@
 (ns cv2.handler
   (:require [compojure.core :refer :all]
+            [cheshire.core :as c]
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
+(defn headerize
+  "wrap HTTP response with status and headers.
+  param package: [operation-successful message-to-send]"
+  [package]
+  (let [[err message] package
+        head {"Content-Type" "application/json; charset=utf-8"}]
+    (if (not err)
+      {:status 400
+       :headers head}
+      {:status 200
+       :headers head
+       :body message})))
+
+(defn jsonize
+  "generate string from jsonizable message
+  param message [operation-successful message-to-send]"
+  [message]
+  (-> message
+      (headerize)
+      (c/generate-string)))
+
 (defroutes app-routes
+  (GET "/tell" [] (jsonize [true "hello, how is everyone?"]))
   (GET "/" [] "Hello World")
   (route/not-found "Not Found"))
 
